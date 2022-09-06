b89432ad2295b53c4ca9e1109bdf411cf329e551
rohansolse <rohansolse@gmail.com>
2022-09-06 11:28:28 +0530

    15 aug changes done

--- test.js
+++ test.js
@@ -1,4 +1,80 @@
-let text = "Mr Blue has a blue house and a blue car.";
-let variable = "blue";
-text = text.replace(new RegExp(variable, "gi"), "red");
-console.log(text);
+// let text = "Mr Blue has a blue house and a blue car.";
+// let variable = "blue";
+// text = text.replace(new RegExp(variable, "gi"), "red");
+// console.log(text);
+
+// function generatePaginationQuery(query, sort, nextKey) {
+//     const sortField = sort == null ? null : sort[0];
+
+//     function nextKeyFn(items) {
+//         if (items.length === 0) {
+//             return null;
+//         }
+
+//         const item = items[items.length - 1];
+
+//         if (sortField == null) {
+//             return { _id: item._id };
+//         }
+
+//         return { _id: item._id, [sortField]: item[sortField] };
+//     }
+
+//     if (nextKey == null) {
+//         return { paginatedQuery: query, nextKeyFn };
+//     }
+
+//     let paginatedQuery = query;
+
+//     if (sort == null) {
+//         paginatedQuery._id = { $gt: nextKey._id };
+//         return { paginatedQuery, nextKey };
+//     }
+
+//     const sortOperator = sort[1] === 1 ? "$gt" : "$lt";
+
+//     const paginationQuery = [
+//         { [sortField]: { [sortOperator]: nextKey[sortField] } },
+//         {
+//             $and: [
+//                 { [sortField]: nextKey[sortField] },
+//                 { _id: { [sortOperator]: nextKey._id } }
+//             ]
+//         }
+//     ];
+
+//     if (paginatedQuery.$or == null) {
+//         paginatedQuery.$or = paginationQuery;
+//     } else {
+//         paginatedQuery = { $and: [query, { $or: paginationQuery }] };
+//     }
+
+//     return { paginatedQuery, nextKeyFn };
+// }
+
+// let data = {
+//     type: "equal",
+//     SAL_VALUE: "sample"
+// };
+
+// let keys = Object.keys(data);
+// let values = Object.values(data);
+
+// console.log(keys, values);
+
+// const foundIndex = keys.findIndex(key => key === "type");
+// const dataObject = {};
+// if (foundIndex === 0) {
+//     dataObject.columeName = keys[1];
+//     dataObject.columeValue = values[1];
+// } else {
+//     dataObject.columeName = keys[0];
+//     dataObject.columeValue = values[0];
+// }
+
+// console.log(dataObject);
+// console.log(foundIndex);
+
+const data = {};
+data.type = "equal";
+console.log(data);
